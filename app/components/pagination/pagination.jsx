6d87efe14e5896b2a10f9a04d22b5e9f249657eb
sabbir-sabbir <iosabbir7890@gmail.com>
2025-03-16 22:43:21 +0600

    add

--- app/components/pagination/pagination.jsx
+++ app/components/pagination/pagination.jsx
@@ -1,9 +1,17 @@
+"use client"
+import { useState, useEffect } from "react"
+
 export default function Pagination({showPerPage}){
+    const [counter, setCounter] = useState(1);
+
+    useEffect(()=> {
+
+    }, [counter])
     return (
         <>
         <div className=" w-full flex justify-between items-center px-20 ">
-            <div><button className="">previews</button></div>
-            <div><button className="">next</button></div>
+            <div><button onClick={()=> setCounter(counter + 1)} className="">previews</button></div>
+            <div><button onClick={()=> setCounter(counter - 1)} className="">next</button></div>
         </div>
         </>
     )
